2ebf46c681a7f50696a60b509983240595f8e3b5
Daniel Palamountain <daniel.palamoun@ad.aie.edu>
2016-09-28 11:29:59 +0930

    Added On_Startup script. Exploded prefab now explodes on spawn

---
+++ Assets/Scripts/Events/On_Startup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/* DESCRIPTION:
+ * Run specified scripts on startup
+ */
+
+public class On_Startup : MonoBehaviour {
+
+	public List<MBAction> Actions = new List<MBAction>();			// List of actions to execute on collision
+
+	void Start () 
+	{
+		foreach (MBAction action in Actions) 
+		{
+			action.Execute();
+		}
+	}
+}
