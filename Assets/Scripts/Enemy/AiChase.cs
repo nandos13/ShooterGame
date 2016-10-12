60c9a635187c048ebd9ecbfed0f7804059b8e1da
Damian Castle <damian.castle@ad.aie.edu>
2016-10-12 12:12:52 +1030

    Added Enemy capsules to chase "Player" script

---
+++ Assets/Scripts/Enemy/AiChase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AiChase : MonoBehaviour {
+
+    private GameObject target;
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.transform.position;
+    }
+}
+
