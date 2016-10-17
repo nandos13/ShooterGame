8d803490a683ba3646988c6f638c76f52b562d11
Jake Perry <jake.perry@ad.aie.edu>
2016-10-18 09:35:21 +1030

    Added simple script to rotate an object by a speed around selected axis. Used for the crystal above enemy buildings

---
+++ Assets/Scripts/Enemy/Buildings/CrystalRotation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalRotation : MonoBehaviour {
+
+	public float value = 1;
+	public bool x = true;
+	public bool y = true;
+	public bool z = true;
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		if (!Options.Paused)
+		{
+			if (x)
+				transform.Rotate (transform.right, value);
+			if (y)
+				transform.Rotate (transform.up, value);
+			if (z)
+				transform.Rotate (transform.forward, value);
+		}
+	}
+}
