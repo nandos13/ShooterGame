5284fbb11b73b13e1a96e946962ba29e65e0c317
Damian Castle <damian.castle@ad.aie.edu>
2016-10-17 11:42:46 +1030

    Added a Dynamic Menu buttons and fading scripts.

---
+++ Assets/Scripts/dynamic menu/ButtonBranch.cs
@@ -0,0 +1,364 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;   // UI data types such as images and buttons
+using System.Collections.Generic;
+
+
+/*  ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // -- W.I.P
+            Button branch is a tool to open buttons from pressing buttons
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+*/  ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+public class ButtonBranch : MonoBehaviour
+{
+
+    public class ButtonScale
+    {
+
+        enum ScaleMode { MatchWidthHeight, IndependentWidthHeight }
+        ScaleMode mode;
+        Vector2 referenceButtonSize;
+
+        [HideInInspector]
+        public Vector2 referenceScreenSize;
+        public Vector2 newButtonSize;
+
+        public void Initialize(Vector2 refButtonSize, Vector2 refScreenSize, int scaleMode)
+        {
+            mode = (ScaleMode)scaleMode;
+            referenceButtonSize = refButtonSize;
+            referenceScreenSize = refScreenSize;
+            SetNewButtonSize();
+        }
+
+        void SetNewButtonSize()
+        {
+            if (mode == ScaleMode.IndependentWidthHeight)
+            {
+                newButtonSize.x = (referenceButtonSize.x * Screen.width) / referenceScreenSize.x;
+                newButtonSize.y = (referenceButtonSize.y * Screen.height) / referenceScreenSize.y;
+            }
+            else if (mode == ScaleMode.MatchWidthHeight)
+            {
+                newButtonSize.x = (referenceButtonSize.x * Screen.width) / referenceScreenSize.x;
+                newButtonSize.y = newButtonSize.x;
+            }
+        }
+    }
+
+    [System.Serializable]   // class and the members can be seen from inspector
+    public class RevealSettings
+    {
+        public enum RevealOption { Linear, Circular };
+        public RevealOption option;
+        public float translateSmooth = 5f;   // the speed of the button to move to its position
+        public float fadeSmooth = 0.01f;    // how fast it fades in
+        public bool revealOnStart = false;
+
+        [HideInInspector]   // hides the next line of code from the inspector
+        public bool opening = false;
+        [HideInInspector]
+        public bool spawned = false;
+    }
+
+    [System.Serializable]
+    public class LinearSpawner
+    {
+        public enum RevealStyle { SlidetToPosition, FadeInAtPosition };
+        public RevealStyle revealStyle;
+        public Vector2 direction = new Vector2(0, 1);   // slide down
+        public float baseButtonSpacing = 5f;    // how much space between each button
+        public int buttonNumOffset = 0;     // how many button spaces offset? sometimes necessary when using multiple button branches
+
+        [HideInInspector]
+        public float buttonSpacing = 5f;
+        public void FitSpacingToScreenSize(Vector2 refScreenSize)
+        {
+            float refScreenFloat = (refScreenSize.x + refScreenSize.y) / 2;
+            float screenFloat = (Screen.width + Screen.height) / 2;
+            buttonSpacing = (baseButtonSpacing * screenFloat) / refScreenFloat;
+        }
+
+    }
+
+    [System.Serializable]
+    public class CircularSpawner
+    {
+        public enum RevealStyle { SlideToPosition, FadeInAtPosition };
+        public RevealStyle revealStyle;
+        public Angle angle;
+        public float baseDistFromBranch = 20;
+
+        [HideInInspector]
+        public float distFromBranch = 0;
+
+        [System.Serializable]
+        public struct Angle { public float minAngle; public float maxAngle; }
+        public void FitDistanceToScreenSize(Vector2 refScreenSize)
+        {
+            float refScreenFloat = (refScreenSize.x + refScreenSize.y) / 2;
+            float screenFloat = (Screen.width + Screen.height) / 2;
+            distFromBranch = (baseDistFromBranch * screenFloat) / refScreenFloat;
+        }
+
+    }
+
+    public GameObject[] buttonRefs;
+    public enum ScaleMode { MatchWidthHeight, IndependentWidthHeight };
+    public ScaleMode mode;
+    public Vector2 referenceButtonSize;
+    public Vector2 referenceScreenSize;
+    ButtonScale buttonScale = new ButtonScale();
+    public RevealSettings revealSettings = new RevealSettings();
+    public LinearSpawner linSpawner = new LinearSpawner();
+    public CircularSpawner circSpawner = new CircularSpawner();
+    [HideInInspector]
+    public List<GameObject> buttons;
+    float lastScreenWidth = 0f;
+    float lastScreenHeight = 0f;
+
+
+
+    void Start()
+    {
+        buttons = new List<GameObject>();
+        buttonScale = new ButtonScale();
+        lastScreenWidth = Screen.width;
+        lastScreenHeight = Screen.height;
+        buttonScale.Initialize(referenceButtonSize, referenceScreenSize, (int)mode);
+        circSpawner.FitDistanceToScreenSize(buttonScale.referenceScreenSize);
+        linSpawner.FitSpacingToScreenSize(buttonScale.referenceScreenSize);
+        if (revealSettings.revealOnStart)
+        {
+            SpawnButtons();
+        }
+    }
+
+
+    void Update()
+    {
+        if (Screen.width != lastScreenWidth || Screen.height != lastScreenHeight)
+        {
+            lastScreenWidth = Screen.width;
+            lastScreenHeight = Screen.height;
+            buttonScale.Initialize(referenceButtonSize, referenceScreenSize, (int)mode);
+            circSpawner.FitDistanceToScreenSize(buttonScale.referenceScreenSize);
+            linSpawner.FitSpacingToScreenSize(buttonScale.referenceScreenSize);
+            SpawnButtons();
+        }
+        if (revealSettings.opening)
+        {
+            if (revealSettings.spawned)
+                SpawnButtons();
+
+            switch (revealSettings.option)
+            {
+                case RevealSettings.RevealOption.Linear:
+                    switch (linSpawner.revealStyle)
+                    {
+                        case LinearSpawner.RevealStyle.SlidetToPosition:
+                            RevealLinearlyNormal();
+                            break;
+                        case LinearSpawner.RevealStyle.FadeInAtPosition:
+                            RevealLinearlyFade();
+                            break;
+                    }
+                    break;
+                case RevealSettings.RevealOption.Circular:
+                    switch (circSpawner.revealStyle)
+                    {
+                        case CircularSpawner.RevealStyle.SlideToPosition:
+                            RevealLinearlyNormal();
+                            break;
+                        case CircularSpawner.RevealStyle.FadeInAtPosition:
+                            RevealLinearlyFade();
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+    public void SpawnButtons()  // if revealOnStart == false, this method will be called by the buttom click event
+    {
+        revealSettings.opening = true;
+        //clear button list, in case there are some already in it
+        for (int i = buttons.Count - 1; i >= 0; i--)
+            Destroy(buttons[i]);
+        buttons.Clear();
+        //clear buttons on any other button branch that has the same parent as this branch
+        ClearCommonButtonBranch();
+
+        for (int i = 0; i < buttonRefs.Length; i++)
+        {
+            GameObject b = Instantiate(buttonRefs[i] as GameObject);
+            b.transform.SetParent(transform); // makes button child of button branch
+            b.transform.position = transform.position;  //  zeroing the position places the button on the button branch
+            // check if button will fade or not
+            if (linSpawner.revealStyle == LinearSpawner.RevealStyle.FadeInAtPosition || circSpawner.revealStyle == CircularSpawner.RevealStyle.FadeInAtPosition)
+            {
+                // change colour alpha of button and its text to 0;
+                Color c = b.GetComponent<Image>().color;
+                c.a = 0;
+                b.GetComponent<Image>().color = c;
+                if (b.GetComponentInChildren<Text>()) // button may not have a text component
+                {
+                    c = b.GetComponentInChildren<Text>().color;
+                    c.a = 0;
+                    b.GetComponentInChildren<Text>().color = c;
+                }
+            }
+            buttons.Add(b);
+        }
+        revealSettings.spawned = true;
+    }
+
+    void RevealLinearlyNormal()
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            //give the button a position to move toward
+            Vector3 targetPos;
+            RectTransform buttonRect = buttons[i].GetComponent<RectTransform>();
+            // set size
+            buttonRect.sizeDelta = new Vector2(buttonScale.newButtonSize.x, buttonScale.newButtonSize.y);
+            // set position
+            targetPos.x = linSpawner.direction.x * ((i + linSpawner.buttonNumOffset) * (buttonRect.sizeDelta.x + linSpawner.buttonSpacing)) + transform.position.x;
+            targetPos.y = linSpawner.direction.y * ((i + linSpawner.buttonNumOffset) * (buttonRect.sizeDelta.y + linSpawner.buttonSpacing)) + transform.position.y;
+            targetPos.z = 0;
+            buttonRect.position = Vector3.Lerp(buttonRect.position, targetPos, revealSettings.translateSmooth * Time.deltaTime);
+        }
+    }
+
+    void RevealLinearlyFade()
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            //give the button a position to move toward
+            Vector3 targetPos;
+            RectTransform buttonRect = buttons[i].GetComponent<RectTransform>();
+            // set size
+            buttonRect.sizeDelta = new Vector2(buttonScale.newButtonSize.x, buttonScale.newButtonSize.y);
+            // set position
+            targetPos.x = linSpawner.direction.x * ((i + linSpawner.buttonNumOffset) * (buttonRect.sizeDelta.x + linSpawner.buttonSpacing)) + transform.position.x;
+            targetPos.y = linSpawner.direction.y * ((i + linSpawner.buttonNumOffset) * (buttonRect.sizeDelta.y + linSpawner.buttonSpacing)) + transform.position.y;
+            targetPos.z = 0;
+            ButtonFader previousButtonFader;
+            if (i > 0)
+                previousButtonFader = buttons[i - 1].GetComponent<ButtonFader>();
+            else
+                previousButtonFader = null;
+            ButtonFader buttonFader = buttons[i].GetComponent<ButtonFader>();
+            if (previousButtonFader)    // first button wont have a previous button
+            {
+                if (previousButtonFader.faded)
+                {
+                    buttons[i].transform.position = targetPos;
+                    if (buttonFader)
+                        buttonFader.Fade(revealSettings.fadeSmooth);
+                    else
+                        Debug.LogError("You need a ButtonFader script to be attached");
+                }
+            }
+            else
+            {
+                buttons[i].transform.position = targetPos;
+                if (buttonFader)
+                    buttonFader.Fade(revealSettings.fadeSmooth);    // for the first button in the array
+                else
+                    Debug.LogError("You need a ButtonFader script to be attached");
+            }
+        }
+    }
+
+    void RevealCircularNormal()
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            // find angle
+            float angleDist = circSpawner.angle.maxAngle - circSpawner.angle.minAngle;
+            float targetAngle = circSpawner.angle.minAngle + (angleDist / buttons.Count) * i;
+            // find pos
+            Vector3 targetPos = transform.position + Vector3.right * circSpawner.distFromBranch;
+            targetPos = RotatePointAroundPivot(targetPos, transform.position, targetAngle);
+            RectTransform buttonRect = buttons[i].GetComponent<RectTransform>();
+            //resize button
+            buttonRect.sizeDelta = new Vector2(buttonScale.newButtonSize.x, buttonScale.newButtonSize.y);
+            buttonRect.position = Vector3.Lerp(buttonRect.position, targetPos, revealSettings.translateSmooth * Time.deltaTime);
+        }
+    }
+
+    void RevealCircularFade()
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            //find angle
+            float angleDist = circSpawner.angle.maxAngle - circSpawner.angle.minAngle;
+            float targetAngle = circSpawner.angle.minAngle + (angleDist / buttons.Count) * i;
+            // find pos
+            Vector3 targetPos = transform.position + Vector3.right * circSpawner.distFromBranch;
+            targetPos = RotatePointAroundPivot(targetPos, transform.position, targetAngle);
+            RectTransform buttonRect = buttons[i].GetComponent<RectTransform>();
+            //resize button
+            buttonRect.sizeDelta = new Vector2(buttonScale.newButtonSize.x, buttonScale.newButtonSize.y);
+
+            ButtonFader previousButtonFader;
+            if (i > 0)
+                previousButtonFader = buttons[i - 1].GetComponent<ButtonFader>();
+            else
+                previousButtonFader = null;
+            ButtonFader buttonFader = buttons[i].GetComponent<ButtonFader>();
+            if (previousButtonFader)    // first button wont have a previous button
+            {
+                if (previousButtonFader.faded)
+                {
+                    buttons[i].transform.position = targetPos;
+                    if (buttonFader)
+                        buttonFader.Fade(revealSettings.fadeSmooth);
+                    else
+                        Debug.LogError("You need a ButtonFader script to be attached");
+                }
+            }
+
+            else
+            {
+                buttons[i].transform.position = targetPos;
+                if (buttonFader)
+                    buttonFader.Fade(revealSettings.fadeSmooth);    // for the first button in the array
+                else
+                    Debug.LogError("You need a ButtonFader script to be attached");
+            }
+        }
+    }
+
+    Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, float angle)
+    {
+        Vector3 targetPoint = point - point;
+        targetPoint = Quaternion.Euler(0, 0, angle) * targetPoint;
+        targetPoint += pivot;
+        return targetPoint;
+    }
+
+    void ClearCommonButtonBranch()
+    {
+        GameObject[] branchers = GameObject.FindGameObjectsWithTag("ButtonBranch");
+        foreach (GameObject branch in branchers)
+        {
+            // check if the parent of this branch is the same as the branch we are currently looking at
+            if (branch.transform.parent == transform.parent)
+            {
+                //removes the branchers button to keep things tidy
+                ButtonBranch bb = branch.GetComponent<ButtonBranch>();
+                for (int i = bb.buttons.Count - 1; i >= 0; i--)
+                    Destroy(bb.buttons[i]);
+                bb.buttons.Clear();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/dynamic menu/Tooltip.cs
@@ -0,0 +1,228 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;   // UI data types such as images and buttons
+
+/*  ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    //-- need any help, ask me: damian
+            Script lets us have images/text box open with a different animations
+
+            3 types :   Width to Height ~ opens image width 1st then drops open height
+                        Height to Width ~ opens image Height 1st then scrolls across to width
+                        Height and Width ~ opens from achors Height and width at same time and expands open       
+
+    ~ set anchors to topleft of textbox, so textbox animates from there to textbox size.
+    ~ set "Open Style" values.
+    ~ (helpful tip) Width and Height values in rect transform should match "Open Box Size" values.
+    ~ drag TextBox Object into UI settings "Text Box", drag Text Object into UI settings "Text".
+    ~ click '+' in On Click() - drag ToolTip object, set it to ToolTip.StartOpen()
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+*/  ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+public class Tooltip : MonoBehaviour
+{
+    [System.Serializable]   // serializes the class, keeps it tidy in unity
+    public class AnimationSettings  // contains settings for UI and how it changes during runtime
+    {
+        public enum OpenStyle { WidthToHeight, HeightToWidth, HeightAndWidth };
+        [Tooltip("Type of animation when opening")]
+        public OpenStyle openStyle; // how the tool tip opens
+        [Range(0.0f, 100.0f), Tooltip("Speed of textbox when it opens")]    // adds slider in unity
+        public float widthSmooth = 0.1f, heightSmooth = 0.1f;   // speed of textbox when it opens
+        [Range(0.0f, 100.0f), Tooltip("How fast text will appear")]     // adds slider in unity
+        public float textSmooth = 0.1f; // how fast text will appear
+        [HideInInspector]   // only works for the next line of code, hide the boolean values
+        public bool widthOpen = false, heightOpen = false; // opens one at a time, we need to know when each is done opening
+        public void Initialize()    // initialize is called when amination starts over
+        {
+            widthOpen = false;
+            heightOpen = false;
+        }
+    }
+
+    [System.Serializable]
+    public class UIsettings
+    {
+        [Tooltip("Drag and drop child's image of button")]
+        public Image textBox;   // will contain text content
+        [Tooltip("Drag and drop child's text of image")]
+        public Text text;   // tooltip message
+        public Vector2 initialBoxSize = new Vector2(0.25f, 0.25f);    // size of textbox without any scaling
+        [Tooltip("Opened box size should match Rect Transform's value")]
+        public Vector2 openedBoxSize = new Vector2(400, 400);
+        public float snapToSizeDistance = 0.2f; //distance from current size to target size before current is snapped to target
+        [Range(0.0f, 100.0f), Tooltip("Time of text image stays open")]     // adds slider in unity
+        public float lifeSpan = 1;
+        // these next varibles dont need to be seen from the inspector, but are public
+        [HideInInspector]
+        public bool opening = false;    // starts when opening is false
+        [HideInInspector]
+        public Color textColor;     // text colour reference
+        [HideInInspector]
+        public Color textBoxColor;
+        [HideInInspector]
+        public RectTransform textBoxRect;   // lets us modify the size of textbox
+        [HideInInspector]
+        public Vector2 currentSize;     // text box sizing
+        public void Initialize()    // initialize our values for animation cycle
+        {
+            textBoxRect = textBox.GetComponent<RectTransform>();
+            textBoxRect.sizeDelta = initialBoxSize;     // is a vector2 size of the rect with respec to its anchor
+            currentSize = textBoxRect.sizeDelta;
+            opening = false;
+            textColor = text.color;
+            textColor.a = 0;
+            text.color = textColor;
+            textBoxColor = textBox.color;
+            textBoxColor.a = 1;
+            textBox.color = textBoxColor;
+            textBox.gameObject.SetActive(false);
+            text.gameObject.SetActive(false);
+        }
+    }
+
+    public AnimationSettings animateSettings = new AnimationSettings();
+    public UIsettings uiSettings = new UIsettings();
+    float lifeTimer = 0;
+
+    void Start()
+    {
+        animateSettings.Initialize();
+        uiSettings.Initialize();
+    }
+
+    public void StartOpen()     // method that calls when button is clicked
+    {
+        uiSettings.opening = true;
+        uiSettings.textBox.gameObject.SetActive(true);
+        uiSettings.text.gameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (uiSettings.opening)
+        {
+            OpenToolTip();
+            if (animateSettings.widthOpen && animateSettings.heightOpen)
+            {
+                lifeTimer += Time.deltaTime;
+                if (lifeTimer > uiSettings.lifeSpan)
+                {
+                    FadeToolTipOut();
+                }
+                else
+                {
+                    FadeTextIn();
+                }
+            }
+        }
+    }
+
+    void OpenToolTip()
+    {
+        // what open style to be used
+        switch (animateSettings.openStyle)
+        {
+            case AnimationSettings.OpenStyle.WidthToHeight:
+                OpenWidthToHeight();
+                break;
+            case AnimationSettings.OpenStyle.HeightToWidth:
+                OpenHeightToWidth();
+                break;
+            case AnimationSettings.OpenStyle.HeightAndWidth:
+                OpenHeightAndWidth();
+                break;
+            default:
+                Debug.LogError("Open animation is not set");
+                break;
+        }
+        uiSettings.textBoxRect.sizeDelta = uiSettings.currentSize;
+    }
+
+    void OpenWidthToHeight()
+    {
+        if (!animateSettings.widthOpen)
+        {
+            OpenWidth();
+        }
+        else
+        {
+            OpenHeight();
+        }
+    }
+
+    void OpenHeightToWidth()
+    {
+        if (!animateSettings.heightOpen)
+        {
+            OpenHeight();
+        }
+        else
+        {
+            OpenWidth();
+        }
+    }
+
+    void OpenHeightAndWidth()
+    {
+        if (!animateSettings.widthOpen)
+        {
+            OpenWidth();
+        }
+        if (!animateSettings.heightOpen)
+        {
+            OpenHeight();
+        }
+    }
+
+    void OpenWidth()
+    {
+        uiSettings.currentSize.x = Mathf.Lerp(uiSettings.currentSize.x, uiSettings.openedBoxSize.x, animateSettings.widthSmooth * Time.deltaTime);
+
+        if (Mathf.Abs(uiSettings.currentSize.x - uiSettings.openedBoxSize.x) < uiSettings.snapToSizeDistance)
+        {
+            uiSettings.currentSize.x = uiSettings.openedBoxSize.x;
+            animateSettings.widthOpen = true;
+        }
+    }
+
+    void OpenHeight()
+    {
+        uiSettings.currentSize.y = Mathf.Lerp(uiSettings.currentSize.y, uiSettings.openedBoxSize.y, animateSettings.heightSmooth * Time.deltaTime);
+
+        if (Mathf.Abs(uiSettings.currentSize.y - uiSettings.openedBoxSize.y) < uiSettings.snapToSizeDistance)
+        {
+            uiSettings.currentSize.y = uiSettings.openedBoxSize.y;
+            animateSettings.heightOpen = true;
+        }
+    }
+
+    void FadeTextIn()
+    {
+        uiSettings.textColor.a = Mathf.Lerp(uiSettings.textColor.a, 1, animateSettings.textSmooth * Time.deltaTime);
+        uiSettings.text.color = uiSettings.textColor;
+    }
+
+    void FadeToolTipOut()
+    {
+        uiSettings.textColor.a = Mathf.Lerp(uiSettings.textColor.a, 0, animateSettings.textSmooth * Time.deltaTime);
+        uiSettings.text.color = uiSettings.textColor;
+        uiSettings.textBoxColor.a = Mathf.Lerp(uiSettings.textBoxColor.a, 0, animateSettings.textSmooth * Time.deltaTime);
+        uiSettings.textBox.color = uiSettings.textBoxColor;
+        if (uiSettings.textBoxColor.a <= 0.01)   // Animation finished and reinitialize
+        {
+            uiSettings.opening = false;
+            animateSettings.Initialize();
+            uiSettings.Initialize();
+            lifeTimer = 0;
+        }
+    }
+}
+
