f7bf512b758f450bfa87856d0e76cfb1c8d37b45
Jake Perry <jake.perry@ad.aie.edu>
2016-10-17 17:03:40 +1030

    Implemented score using a script AddScore. Added canvas prefab which displays score. Uses a script to refresh each frame.

---
+++ Assets/Scripts/Actions/AddScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* DESCRIPTION:
+ * Adds score when executed.
+ */
+
+public class AddScore : MBAction
+{
+	[Range (-1000, 1000)]
+	public int score = 0;
+
+	public override void Execute ()
+	{
+		GameStatistics.AddScore(score);
+	}
+}
---
+++ Assets/Scripts/UI/RefreshScore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/* DESCRIPTION:
+ * Updates the score each frame
+ */
+
+public class RefreshScore : MonoBehaviour {
+
+	public Text scoreText;
+
+	void Start () 
+	{
+	}
+
+	void Update () 
+	{
+		scoreText.text = "Score: " + GameStatistics.Score.ToString();
+	}
+}
