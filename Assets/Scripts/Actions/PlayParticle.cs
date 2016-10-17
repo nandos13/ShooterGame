1c235191f664a38b1a1853237dc8a6f935189e91
Jake Perry <jake.perry@ad.aie.edu>
2016-10-17 12:25:56 +1030

    Added Play particle script. Will play a particle system once

---
+++ Assets/Scripts/Actions/PlayParticle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayParticle : MBAction {
+
+	public ParticleSystem particles;
+
+	public override void Execute ()
+	{
+		if (particles)
+		{
+			if (collision.collider)
+				particles.transform.position = collision.contacts[0].point;
+			particles.Play();
+		}
+	}
+}
