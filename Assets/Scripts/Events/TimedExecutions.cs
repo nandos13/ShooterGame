1cd4dd87eeb9ceeebec5709ebc43832abffa55f7
Jake Perry <jake.perry@ad.aie.edu>
2016-10-18 17:13:15 +1030

    Began working on Timed executions script + custom editor for the class. This will allow a list of actions to be run chronologically with a time delay. Editor does not seem to be saving once the scene is played. this needs to be fixed.

---
+++ Assets/Scripts/Events/TimedExecutions.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ActionValue
+{
+	public MBAction action;
+	public float value = 0;
+
+	public ActionValue()
+	{
+		// Constructor
+		value = 0;
+	}
+}
+
+public class TimedExecutions : MonoBehaviour {
+
+	public List<ActionValue> actions = new List<ActionValue>();
+
+	void Start () 
+	{
+		StartCoroutine(Run());
+	}
+
+	IEnumerator Run ()
+	{
+		foreach (ActionValue av in actions)
+		{
+			// Wait for value time
+			yield return new WaitForSeconds (av.value);
+
+			// Execute action
+			av.action.Execute();
+		}
+	}
+}
