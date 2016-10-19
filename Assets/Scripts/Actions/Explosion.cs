ddef056cb698e8b6da152910edff7c3b4d4357c8
Jake Perry <jake.perry@ad.aie.edu>
2016-10-19 13:06:21 +1030

    Fixed the way explosions apply damage so they should now affect target objects that have the health script on a gameobject without a collider.

--- Assets/Scripts/Actions/Explosion.cs
+++ Assets/Scripts/Actions/Explosion.cs
@@ -11,7 +11,8 @@ public class Explosion : MBAction {
 	[Range(0.0f, 100.0f)]
 	public float Radius = 10;
 	public float Damage = 2;
-	public List<string> CollisionTags = new List<string>();			// A list of collisions to ignore
+	public List<Health> affectedHealthComponents = new List<Health>();		// Tracks which components have already been affected by this explosion
+	public List<string> CollisionTags = new List<string>();					// A list of collisions to ignore
 	public COLLISION_MODE mode = COLLISION_MODE.IgnoreSelected;
 
 	public override void Execute ()
@@ -55,21 +56,42 @@ public class Explosion : MBAction {
 			{
 				// Apply the explosion here
 				Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
-				Health h = col.gameObject.GetComponent<Health>();
+				Health h = col.gameObject.GetComponentInParent<Health>();
+				//Debug.Log("got here: " + col.gameObject.name);
 
 				if (rb)
 					rb.AddExplosionForce (ExplosiveForce, transform.position, Radius, JumpModifier, ForceMode.Impulse);
 
 				if (h && Damage != 0)
 				{
-					// Calculate the damage based on the distance from the explosion
-					float damageMultiplier = Vector3.Distance (transform.position, col.transform.position) / Radius;
+					/* Check if the current health module has already been hit by this explosion.
+					 * If it has not, add the component to the list. */
+					bool affected = false;
+					foreach (Health hc in affectedHealthComponents)
+					{
+						if (hc == h)
+						{
+							affected = true;
+							break;
+						}
+					}
 
-					h.ApplyDamage (Damage * damageMultiplier);
+					if (!affected)
+					{
+						// Calculate the damage based on the distance from the explosion
+						float damageMultiplier = 1 - (Vector3.Distance (transform.position, col.transform.position) / Radius);
+						h.ApplyDamage (Damage * damageMultiplier);
+
+						// Add the component to the list so it is not damaged twice by a single explosion
+						affectedHealthComponents.Add(h);
+					}
 				}
 					
 				// TODO: Add damage based on force
 			}
 		}
+
+		// Clear the health tracking list
+		affectedHealthComponents.Clear();
 	}
 }
