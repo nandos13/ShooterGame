4b46d70f965afd2b036fe81a063c26593946f32a
Damian Castle <damian.castle@hotmail.com>
2016-10-20 12:23:08 +1030

    Updated Enemies AI script, now has Investigate when player gets near.

--- Assets/Scripts/Enemy/AIEnemyControl.cs
+++ Assets/Scripts/Enemy/AIEnemyControl.cs
@@ -3,6 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
+
+/*
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // -- WIP
+        AI script, Enemies patrol around map to set waypoints, Also has a Alert investigation, 
+        where AI stops and waits a set time and goes back to patroling, unless player goes within 
+        aggro range where Enemy will chase player. also has a lose aggro if player runs way far enough.
+
+    ~   Add 'Size' of waypoints, drag waypoint objects into Element slot/s
+    ~   Drag Player prefab into 'Target' slot
+
+    // -- things to still add
+
+    ~   Aggro on damaging AI
+    ~   (BAF)Bring-A-Friend mechanics, attack an AI, and all surrounding AI will aggro too.
+    ~   (LOS)Line-Of_sight mechanics, if behind a wall and out of sight but within range of AI,
+            Ai will not Aggro.
+
+    // -- Note: Did a stress test with 120 AI characters and had no lag problems.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+*/
+
+
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
     public class AIEnemyControl : MonoBehaviour
@@ -13,24 +40,41 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         public enum State
         {
             PATROL,
-            CHASE
+            CHASE,
+            INVESTIGATE
         }
 
         public State state;
-        private bool alive;
+        //private bool alive;
 
         // var for patrol
         public GameObject[] waypoints;
         private int waypointInd = 0;
+        [Range(0, 20)]
         public float patrolSpeed = 0.5f;
 
         // var for chasing
-        public float chaseSpeed = 1f;
-        public float aggroRange = 20f;
-        public float deAggro = 40f;
+        [Range(0, 20)]
+        public float chaseSpeed = 1.0f;
+        [Range(0, 200)]
+        public float aggroRange = 15.5f;
+        [Range(0, 200)]
+        public float deAggro = 40.0f;
         public GameObject target;
 
-         
+        // var for investigation
+        private Vector3 investigateSpot;
+        private float timer = 0;
+        [Range(0, 50)]
+        public float investigateWait = 10;
+
+        // var for sight
+        public float heightMultiplier;
+        [Range(0, 200)]
+        public float sightDist = 20.0f;
+        
+
+
         void Start()
         {
             agent = GetComponent<NavMeshAgent>();
@@ -40,15 +84,19 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             agent.updateRotation = false;
 
             state = AIEnemyControl.State.PATROL;
-            alive = true;
+            heightMultiplier = 4.22f;
+            StartCoroutine("FSM");
+            //alive = true;
         }
 
         void Update()
         {
-            if (Vector3.Distance(target.transform.position, transform.position) <= aggroRange)
-                state = AIEnemyControl.State.CHASE;
-            else if (Vector3.Distance(target.transform.position, transform.position) >= deAggro)
+            if(Vector3.Distance(target.transform.position, transform.position) >= deAggro)
                 state = AIEnemyControl.State.PATROL;
+            else if (Vector3.Distance(target.transform.position, transform.position) <= aggroRange)
+                state = AIEnemyControl.State.CHASE;
+            else if (Vector3.Distance(target.transform.position, transform.position) <= sightDist )
+                state = AIEnemyControl.State.INVESTIGATE;
             FSM();
         }
         
@@ -61,6 +109,9 @@ namespace UnityStandardAssets.Characters.ThirdPerson
                     break;
                 case State.CHASE:
                     Chase();
+                    break;
+                case State.INVESTIGATE:
+                    Investigate();
                     break;
             }
         }
@@ -90,5 +141,64 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             character.Move(agent.desiredVelocity, false, false);
 
         }
+
+        void Investigate()
+        {
+            timer += Time.deltaTime;
+
+            agent.SetDestination(this.transform.position);
+            character.Move(Vector3.zero, false, false);
+            transform.LookAt(investigateSpot);
+            if (timer >= investigateWait)
+            {
+                state = AIEnemyControl.State.PATROL;
+                timer = 0;
+            }
+        }
+
+        //void OnTriggerEnter(Collider coll)
+        //{
+        //    if (coll.tag == "Player")
+        //    {
+        //        state = AIEnemyControl.State.INVESTIGATE;
+        //        investigateSpot = coll.gameObject.transform.position;
+        //    }
+        //}
+
+        void FixedUpdate()
+        {
+            RaycastHit hit;
+            Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, transform.forward * sightDist, Color.green);
+            Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (transform.forward + transform.right).normalized * sightDist, Color.green);
+            Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (transform.forward - transform.right).normalized * sightDist, Color.green);
+
+            if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, transform.forward, out hit, sightDist))
+            {
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    state = AIEnemyControl.State.CHASE;
+                    target = hit.collider.gameObject;
+                }
+            }
+
+            if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (transform.forward + transform.right).normalized, out hit, sightDist))
+            {
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    state = AIEnemyControl.State.CHASE;
+                    target = hit.collider.gameObject;
+                }
+            }
+
+            if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (transform.forward - transform.right).normalized, out hit, sightDist))
+            {
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    state = AIEnemyControl.State.CHASE;
+                    target = hit.collider.gameObject;
+                }
+            }
+        }
+
     }
 }
