c80bae7baad1bb8ae541dce31062c0d7d03f256b
Damian Castle <damian.castle@hotmail.com>
2016-10-27 22:10:24 +1030

    Added a custom button script for artist Added a scene changer script

---
+++ Assets/Scripts/UI/ChangeScenes.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections; 
+using UnityEngine.SceneManagement; 
+
+
+//--    This is for swapping scenes, Loading Scene -> Menu Scene -> Game Scene. 
+
+
+public class ChangeScenes : MonoBehaviour
+{
+	public void ChangeToScene (string sceneToChangeTo)
+    {
+        
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+}
