0b9562b59a30541408cc284b42c8bd6a7462aedd
Jake Perry <jake.perry@ad.aie.edu>
2016-11-01 11:21:27 +1030

    On Enable script implemented.

---
+++ Assets/Scripts/Events/On_Enable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class On_Enable : MonoBehaviour {
+
+	public List<MBAction> Actions = new List<MBAction>();			// List of actions to execute on collision
+
+	void OnEnable () 
+	{
+		foreach (MBAction action in Actions) 
+		{
+			if (action)
+				action.Execute();
+		}
+	}
+}
