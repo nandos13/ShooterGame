453c4867bbfe9147d75f82c397c0850f4e317dfe
Jake Perry <jake-perry@live.com>
2016-11-02 13:26:53 +1030

    Added enemy spawning functionality using two scripts. SpawnerHandler script should exist once in the scene, on a Game Manager object. This holds information about spawn points and handles the spawning of objects passed into it. SpawnScript will spawn a specified object when called. Use like all other MBAction scripts, eg. under death scripts in a health script, etc.

---
+++ Assets/Scripts/Enemy/Spawning/SpawnScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* DESCRIPTION:
+ * Spawns an enemy on Execute(), using spawnPoint info from a specified spawnHandler
+ */
+
+public class SpawnScript : MBAction {
+
+	public SpawnerHandler spawnHandler;
+	public GameObject spawnObject;
+	public bool aggroOnSpawn = true;
+
+	void Start () 
+	{
+		if (!spawnHandler)
+			Debug.LogWarning("No spawnHandler specified in SpawnScript on object '" + transform.name + "'. Please drag the spawnerHandler in.");
+
+		if (!spawnObject)
+			Debug.LogWarning("No object specified in SpawnScript on object '" + transform.name + "'. Please drag in the object to be spawned.");
+	}
+
+	public override void Execute () 
+	{
+		if (spawnHandler && spawnObject)
+			spawnHandler.Spawn(spawnObject, aggroOnSpawn);
+	}
+}
---
+++ Assets/Scripts/Enemy/Spawning/SpawnerHandler.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/* DESCRIPTION:
+ * This script contains information about spawn locations in the level. Each level must have an 
+ * instance of this script somewhere in the heirarchy for enemy spawns to work.
+ */
+
+public class SpawnerHandler : MonoBehaviour {
+
+	public static SpawnerHandler instance;							// Only allow a single instance of this script to exist
+	public List<Transform> spawnPoints = new List<Transform>();		// List of spawn points
+
+	private GameObject player;										// Reference to player, used for aggro on spawn
+
+	void Start () 
+	{
+		if (!instance)
+			instance = this;
+		else
+		{
+			Destroy(this);
+			Debug.LogWarning("More than one instance of class SpawnerHandler exists. Deleting duplicate instance.");
+		}
+
+		if (spawnPoints.Count <= 0)
+			Debug.LogWarning("No spawn points specified in SpawnHandler script. No enemies will spawn.");
+
+		// Get player reference
+		player = GameObject.Find ("Player");
+	}
+
+	public void Spawn (GameObject obj, bool aggroPlayer)
+	{
+		/* Spawns a specified object at a random spawn point in the level */
+
+		if (obj  && spawnPoints.Count > 0)
+		{
+			// Instantiate new instance of obj
+			GameObject newObj = Instantiate (obj) as GameObject;
+
+			// Choose random spawn point
+			int i = pickSpawn ();
+
+			newObj.transform.position = spawnPoints[i].position;
+
+			if (aggroPlayer)
+			{
+				// Get attached navmesh agent and set destination to player position
+				NavMeshAgent navAgent = newObj.GetComponent<NavMeshAgent>();
+				if (navAgent)
+				{
+					if (player)
+						navAgent.destination = player.transform.position;
+					else
+						Debug.LogWarning("Could not find reference to player.");
+				}
+				else
+					Debug.LogWarning("Spawned enemy does not have an attached navmesh component and can't aggro the player.");
+			}
+		}
+	}
+
+	private int pickSpawn ()
+	{
+		/* Picks a random spawn point from Transform array spawnPoints */
+
+		removeBlankSpawns();
+		if (spawnPoints.Count >= 0)
+		{
+			int i = -1;
+			while (i < 0)
+			{
+				int index = Random.Range (0, spawnPoints.Count);
+				// Check transform exists
+				if (spawnPoints[index] != null)
+					i = index;
+			}
+			return i;
+		}
+		else
+			return -1;
+	}
+
+	private void removeBlankSpawns ()
+	{
+		/* Iterates through all spawnPoints and removes any null transforms */
+
+		for (int i = 0; i < spawnPoints.Count; i++)
+		{
+			if (spawnPoints[i] == null)
+			{
+				// Remove this index
+				spawnPoints.RemoveAt(i);
+
+				// Set iterator back so nothing is skipped
+				i--;
+			}
+		}
+	}
+}
