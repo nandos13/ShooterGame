630d19b8a6fac12bacacc83cb9ac5760811b299d
Jake Perry <jake.perry@ad.aie.edu>
2016-11-07 16:53:52 +1030

    Finally fixed particles for grenade by adding an option to Emit Particle script to inherit parent transform pos.

--- Assets/Scripts/Actions/EmitParticle.cs
+++ Assets/Scripts/Actions/EmitParticle.cs
@@ -5,12 +5,14 @@ public class EmitParticle : MBAction {
 
 	public ParticleSystem particles;
 	public uint amount = 0;
+	public bool inheritTransformPos = false;
 
 	public override void Execute ()
 	{
 		if (particles)
 		{
-			particles.transform.position = transform.position;
+			if (inheritTransformPos)
+				particles.transform.position = transform.position;
 			if (collision.collider)
 				particles.transform.position = collision.contacts[0].point;
 			particles.Emit((int)amount);
