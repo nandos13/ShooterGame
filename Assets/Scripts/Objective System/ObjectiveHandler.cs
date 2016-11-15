06f94f0df3b2e9fcfa66ac953201b54e4fbf8647
Jake Perry <jake-perry@live.com>
2016-11-15 16:16:16 +1030

    Can now have more than one instance of an objective handler script in the scene.

--- Assets/Scripts/Objective System/ObjectiveHandler.cs
+++ Assets/Scripts/Objective System/ObjectiveHandler.cs
@@ -7,21 +7,12 @@ using System.Collections.Generic;
 
 public class ObjectiveHandler : MonoBehaviour {
 
-	public static ObjectiveHandler instance;								// Only allow a single instance of this script to exist
 	public List<ObjectiveBase> objectives = new List<ObjectiveBase>();		// A list of all objectives
 	public List<MBAction> onPass = new List<MBAction>();					// List of actions executed once all primary objectives are passed
 	private bool passed = false;
 
 	void Awake ()
 	{
-		if (!instance)
-			instance = this;
-		else
-		{
-			Destroy(this);
-			Debug.LogWarning("More than one instance of class ObjectiveHandler exists. Deleting duplicate instance.");
-		}
-
 		if (objectives.Count <= 0)
 			Debug.LogWarning("ObjectiveHandler has no specified objectives.");
 	}
