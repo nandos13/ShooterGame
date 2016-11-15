28c63e35e0a574385899057ea2a4e49042870f72
Jake Perry <jake-perry@live.com>
2016-11-15 16:25:49 +1030

    Added a script to play a sound file

---
+++ Assets/Scripts/Actions/PlaySound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MBAction {
+
+	public AudioSource source;
+	public AudioClip sound;
+
+	void Start ()
+	{
+		if (source)
+			source.playOnAwake = false;
+	}
+
+	public override void Execute () 
+	{
+		if (source && sound)
+			source.PlayOneShot(sound);
+	}
+}
