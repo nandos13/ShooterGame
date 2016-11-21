0535f9b2a0df68ddbb62692f7247cd2488ee5227
Jake Perry <jake-perry@live.com>
2016-11-21 21:56:05 +1030

    Added script to manually set the time scale for the game. Changed the main menu UI.

---
+++ Assets/Scripts/Game Manager/SetTimeScale.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetTimeScale : MBAction {
+
+	[Range (0, 5)]
+	public float scale = 1;
+
+	public override void Execute ()
+	{
+		Time.timeScale = scale;
+	}
+}
